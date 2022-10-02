f36185c323eb5deebbaf96bb5f54a79ac1b76b37
Gwancheol-Lee <70332526+Gwancheol-Lee@users.noreply.github.com>
2022-10-03 00:01:52 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -5,7 +5,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int year = sc.nextInt();
 		
-		if (year % 4 == 0 && (year % 100) != 0) {
+		if (year % 4 == 0 && year % 100 != 0) {
 			System.out.println("1");
 		}else if (year % 4 == 0 && year % 400 == 0){
 			System.out.println("1");

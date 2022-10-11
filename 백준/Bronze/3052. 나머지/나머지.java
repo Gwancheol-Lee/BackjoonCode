44c8ee4be273b3d367bc00dfcdf51bc1ebc66bdc
Gwancheol-Lee <70332526+Gwancheol-Lee@users.noreply.github.com>
2022-10-11 23:56:39 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		Integer[] arr = new Integer[10];
+		
+		for (int i=0; i<arr.length; i++) {
+			int x = Integer.parseInt(br.readLine());
+			arr[i] = x%42;  			
+		}
+		
+		Set<Integer> set = new HashSet<Integer>(Arrays.asList(arr));
+
+		int count = set.size();
+		
+		bw.write(String.valueOf(count));
+		br.close();
+		bw.close();
+	}
+} 
